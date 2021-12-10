68bb234688888db6362fd88dbba037d2282e603f
joaquin <jecorrea@protonmail.com>
2021-12-09 20:54:06 -0500

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :transactions
+  # resources :transactions
+
+  #Custom routes:
+  get '/balance', to: 'transactions#index'
+  post '/add_transaction', to: 'transactions#create'
+  put '/spend_points', to: 'transactions#spend'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
